22b61ceb58e7fccbc39064121b536584a313b164
bigsaigon333 <likepeppermint@gmail.com>
2020-10-16 22:10:59 +0900

    mongodb asyncronous connection

--- app.js
+++ app.js
@@ -9,33 +9,27 @@ dotenv.config();
 const SmbRouter = require("./router/SmbRouter.js");
 const app = express();
 
-(async () => {
-	try {
-		await mongoose.connect(process.env.MONGODB_URL, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			useFindAndModify: false,
-		});
-		console.log("mongoose connection in try");
+mongoose.connect(process.env.MONGODB_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+});
+console.log("mongoose connection in try");
 
-		const db = mongoose.connection;
-		db.on("error", (error) => console.error(error));
-		db.once("open", () => console.log("mongoose connection is established"));
-		console.log("after dbInit");
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("mongoose connection is established"));
+console.log("after dbInit");
 
-		app.use(express.json());
-		app.use(express.urlencoded({ extended: false }));
-		app.use(morgan("dev"));
-		app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+app.use(helmet());
 
-		app.use("/smb", SmbRouter);
-		app.use((req, res) => {
-			// res.status(404).sendFile(path.resolve(__dirname, "src", "404.html"));
-			res.status(404).send("Error");
-		});
+app.use("/smb", SmbRouter);
+app.use((req, res) => {
+	// res.status(404).sendFile(path.resolve(__dirname, "src", "404.html"));
+	res.status(404).send("Error");
+});
 
-		app.listen(3000, () => console.log("Server is listening"));
-	} catch (error) {
-		console.error(error);
-	}
-})();
+app.listen(3000, () => console.log("Server is listening"));
