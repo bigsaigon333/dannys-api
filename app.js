6f46d4fb98c804e70a930af7f07efc510539600c
bigsaigon333 <likepeppermint@gmail.com>
2020-10-17 13:16:12 +0900

    cors  설정 완료

--- app.js
+++ app.js
@@ -2,35 +2,45 @@ const express = require("express");
 const mongoose = require("mongoose");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const path = require("path");
+
+const cors = require("cors");
+
 const dotenv = require("dotenv");
 dotenv.config();
 
-const SmbRouter = require("./router/SmbRouter.js");
 const app = express();
+app.use(cors());
+
+const SmbRouter = require("./router/SmbRouter.js");
+
 (async function () {
-	await mongoose.connect(process.env.MONGODB_URL, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useFindAndModify: false,
-	});
-	console.log("mongoose connection in try");
+	try {
+		await mongoose.connect(process.env.MONGODB_URL, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useFindAndModify: false,
+		});
+		console.log("mongoose connection in try");
 
-	const db = mongoose.connection;
-	db.on("error", (error) => console.error(error));
-	db.once("open", () => console.log("mongoose connection is established"));
-	console.log("after dbInit");
+		const db = mongoose.connection;
+		db.on("error", (error) => console.error(error));
+		db.once("open", () => console.log("mongoose connection is established"));
+		console.log("after dbInit");
 
-	app.use(express.json());
-	app.use(express.urlencoded({ extended: false }));
-	app.use(morgan("dev"));
-	app.use(helmet());
+		app.use(express.json());
+		app.use(express.urlencoded({ extended: false }));
+		app.use(morgan("dev"));
+		app.use(helmet());
 
-	app.use("/smb", SmbRouter);
-	app.use((req, res) => {
-		// res.status(404).sendFile(path.resolve(__dirname, "src", "404.html"));
-		res.status(404).send("Error");
-	});
+		app.use("/smb", SmbRouter);
+		app.use((req, res) => {
+			// res.status(404).sendFile(path.resolve(__dirname, "src", "404.html"));
+			res.status(404).send("Error");
+		});
+		app.listen(process.env.PORT || 3000, () =>
+			console.log("Server is listening")
+		);
+	} catch (error) {
+		console.error(error);
+	}
 })();
-
-app.listen(process.env.PORT || 3000, () => console.log("Server is listening"));
