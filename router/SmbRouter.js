4988f154fd9b3317837c17e2907cafc54a37e5c1
bigsaigon333 <likepeppermint@gmail.com>
2020-11-06 20:33:34 +0900

    jwt accessToken expiresIn 15s -> 15m 수정

--- router/SmbRouter.js
+++ router/SmbRouter.js
@@ -75,7 +75,7 @@ router.post("/login", (req, res) => {
 });
 
 function generateAccessToken(user) {
-	return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15s" });
+	return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15m" });
 }
 
 function authenticateToken(req, res, next) {
